5484d4597a42e167c29bc0301fbf42dd2f024ed7
Paschodes <paschal.c.chikschal.c.chikwendu@gmail.com>
2022-09-09 14:59:44 -0700

    Task 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - Prints random number to variable
  *
- * Return: Always (Success)
+ * Return: Always 0 (Success)
  */
 /* betty style doc for function main goes there */
 int main(void)
@@ -16,17 +16,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("%s %d is %d and is ", last, n, n % 10);
+
 	if (n % 10 > 5)
-	{
 		printf("greater than 5\n");
-	}
-	else if (n % 10 == 0);
-	{
+	else if (n % 10 == 0)
 		printf("0\n");
-	}
 	else
-	{
 		printf("less than 6 and not 0\n");
-	}
 	return (0);
 }
