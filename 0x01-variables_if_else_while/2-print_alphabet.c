0c4b52cea24d4d771cdbb04e3356e7309aa4c6c2
Paschodes <paschal.c.chikschal.c.chikwendu@gmail.com>
2022-09-09 15:11:00 -0700

    Task 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - write a program that prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char paschal;
+
+	for (paschal = 'a'; paschal <= 'z'; paschal++)
+		putchar(love);
+	putchar('\n');
+	return (0);
+}
