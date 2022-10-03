cd1e2a82e67cd310796b5e88924c3d17f764f953
Paschodes <paschal.c.chikschal.c.chikwendu@gmail.com>
2022-10-03 04:15:30 -0700

    Task 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Write a program that prints name, followed by a new line
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", arg[0]);
+
+return (0);
+}
