5c489f64878169d2aded159d12758de3d2202eb4
Paschodes <paschal.c.chikschal.c.chikwendu@gmail.com>
2022-10-06 14:58:15 -0700

    Task 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -36,7 +36,7 @@ void _print(char *str, int l)
  *
  * Return: pointer to dest, or NULL on failure
  */
-char *mul(char n, char *num, int num_index; char *dest, int dest_index)
+char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 {
 	int j, k, mul, mulrem, add, addrem;
 
