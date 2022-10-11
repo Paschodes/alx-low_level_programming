05c2599aeaafff572b32c6d4f92ce1024a66f941
Paschodes <paschal.c.chikschal.c.chikwendu@gmail.com>
2022-10-11 15:34:36 -0700

    Task 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,7 +4,8 @@
  * new_dog - creates a new dog
  * @name: 1st member.
  * @age: 2nd member.
- * @owner: 3rd member
+ * @owner: 3rd member.
+ *
  * Return: NULL if fail.
  */
 dog_t *new_dog(char *name, float age, char *owner)
